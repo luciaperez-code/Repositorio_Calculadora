f8d6e34d727cce9d04b93841fbe290c79278c26e
Davidlara-code <78635056+Davidlara-code@users.noreply.github.com>
2021-03-05 21:48:05 +0100

    commit david

--- Resta.java
+++ Resta.java
@@ -1,6 +1,6 @@
 /**
  * 
- * Esta clase implementará todos los métodos que están relacionados con la resta de una calculadora.
+ * Esta clase implementarï¿½ todos los mï¿½todos que estï¿½n relacionados con la resta de una calculadora.
  * de la calculadora.
  * ESTA CLASE ACEPTA NUMEROS NEGATIVOS
  *
@@ -11,10 +11,13 @@
  */
 public class Resta {
 
+
+
+	// estoy probando cambios y viendo como se sincroniza con GitHub
 		
 	/**
 	 * 
-	 * Método que devuelve la resta de dos numeros reales
+	 * Mï¿½todo que devuelve la resta de dos numeros reales
 	 * 
 	 * @param a variable que utilizo.
 	 * 
@@ -36,7 +39,7 @@ public class Resta {
 
 	/**
 	 * 
-	 * Método que devuelve la resta de numeros reales
+	 * Mï¿½todo que devuelve la resta de numeros reales
 	 * 
 	 * @param a variable que utilizo.
 	 * 
@@ -55,7 +58,7 @@ public class Resta {
 
 	/**
 	 * 
-	 * Método que devuelve la resta de tres numeros reales.
+	 * Mï¿½todo que devuelve la resta de tres numeros reales.
 	 * 
 	 * @param a variable que utilizo.
 	 * 
@@ -75,7 +78,7 @@ public class Resta {
 
 	/**
 	 * 
-	 * Método que devuelve la resta de dos numeros reales acumulados.
+	 * Mï¿½todo que devuelve la resta de dos numeros reales acumulados.
 	 * 
 	 * @param a variable que utilizo.
 	 * 
