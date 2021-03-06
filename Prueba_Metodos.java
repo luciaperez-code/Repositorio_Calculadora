aee641f542535739608e2b282a5931868c372653
luciaperez-code <78657972+luciaperez-code@users.noreply.github.com>
2021-03-06 18:37:49 +0100

    Update Prueba_Metodos.java

--- Prueba_Metodos.java
+++ Prueba_Metodos.java
@@ -4,19 +4,14 @@ public class Prueba_Metodos {
 
 	public static void main(String[] args) {
 		
+	//METODO SUMA
+	System.out.println("Esta es la prueba del metodo suma desde main " + Suma.sumaReal(1.1, 2.2));
 
-		//METODO RESTA
-		
-	
-		
+	//METODO RESTA
 	System.out.println("este es la prueba del metodo resta desde main " + Resta.restar1(2, 4));
 		
-		
-		
 	
-// METODO OPERACION
-	
-			
+	// METODO OPERACION			
 	System.out.println("estoy probando el metodo operacion " +  Operacion.Prueba_porcentaje(10, 5));
 	
 	
